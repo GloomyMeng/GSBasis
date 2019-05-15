19a926460d3fbb7d9804cfac948b84bdf7018a1d
gloomy.meng <gloomy.meng.049@gmail.com>
2019-05-15 17:17:55 +0800

    Updated: change lint at first

--- Scripts/auto_push.sh
+++ Scripts/auto_push.sh
@@ -16,32 +16,30 @@ else
 	Module_Name="${Module_Name%.*}"
 	Old_Version="$(grep -r 's.version* ' ${Module_File} |awk -F = '{print $2}')"
 
-	# 1.更新 jazzy documents
+  #  1. Lint to Cocoapods
+  echo "\n Cocoapods lint..."
+  echo $(cd ${Module_Path}; pod lib lint ${Module_File})
+
+	#  2.更新 jazzy documents
 
 	echo "Begin Generate jazzy Document....\n"
 	echo $(cd ${Module_Code_Path}; jazzy -m ${Module_Name} -x -workspace,${Module_Name}.xcworkspace,-scheme,${Module_Name} -c -a Gloomy.Meng -u https://gloomymeng.io/ -g https://github.com/GloomyMeng -o ${Module_Path}/docs --min-acl internal --exclude=/*/*Typealiases.swift)
 	echo "\nGit Commit For Document Updated "
   echo $(cd ${Module_Path};git add .; git commit -m "Updated: code documents updated")
 
-  # 2.更新 .podspec 版本号
+  #  3.更新 .podspec 版本号
   echo "\nBegin Update Podspec Version...."
   echo $(sed -i "" "s/${Old_Version}/ '${Tag_Version}'/g" ${Module_File})
   echo "\nGit Commit For Podspec Updated"
   echo $(cd ${Module_Path};git add .; git commit -m "Updated: Podspec Version Updated")
 
-
-
-   # 3.GIT PUSH & GIT TAG PUSH
+   #  4.GIT PUSH & GIT TAG PUSH
   echo "\nGit Push...."
   echo $(cd ${Module_Path}; git push)
   echo "\nAdd Tag...."
   echo $(cd ${Module_Path};git tag -a ${Tag_Version} -m 'Release Version For Cocoapods')
   echo "\nGit Push Tag...."
   echo $(cd ${Module_Path}; git push origin ${Tag_Version})
-
-  #  4. Lint to Cocoapods
-  echo "\n Cocoapods lint..."
-  echo $(cd ${Module_Path}; pod lib lint ${Module_File})
 
   #  5. Thunk to Cocoapods
   echo "\n Cocoapods Push..."
